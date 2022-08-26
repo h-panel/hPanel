929356983c0277c7a5d4e437bed04088d1003711
badsk <103528714+b4dsk@users.noreply.github.com>
2022-08-26 16:28:43 +0100

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,18 +7,18 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>{{ config('app.name', 'Jexactyl') }} - @yield('title')</title>
+        <title>hPanel</title>
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
         <meta name="_token" content="{{ csrf_token() }}">
-
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
-        <link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32">
-        <link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16">
         <link rel="manifest" href="/favicons/manifest.json">
         <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#bc6e3c">
-        <link rel="shortcut icon" href="/favicons/favicon.ico">
+        <link rel="shortcut icon" href="https://cdn.discordapp.com/attachments/987734229469253674/1012679062889697330/Purple_Dark_Blue_Modern_Letter_H_Logo_Technology.png">
         <meta name="msapplication-config" content="/favicons/browserconfig.xml">
         <meta name="theme-color" content="#0e4688">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
         <script src="https://unpkg.com/feather-icons"></script>
 
@@ -406,7 +406,7 @@ pre {
     border-color: hsl(209, 20%, 25%);
 }
 </style>
-    <body class="skin-blue fixed">
+    <body style="font-family: 'Inter', sans-serif;" class="skin-blue fixed">
         <div class="wrapper">
             <header class="main-header">
                 <a style="border-top: 5px;" href="{{ route('index') }}" class="logo">
