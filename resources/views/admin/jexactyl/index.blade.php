9036e7ce6dd20cd19406ee7f47263a7bf2d54187
badsk <103528714+b4dsk@users.noreply.github.com>
2022-08-26 14:39:00 +0100

    Update index.blade.php

--- resources/views/admin/jexactyl/index.blade.php
+++ resources/views/admin/jexactyl/index.blade.php
@@ -2,11 +2,11 @@
 @include('partials/admin.jexactyl.nav', ['activeTab' => 'index'])
 
 @section('title')
-    Jexactyl Settings
+    hPanel Settings
 @endsection
 
 @section('content-header')
-    <h1>Jexactyl Settings<small>Configure Jexactyl-specific settings for the Panel.</small></h1>
+    <h1>hPanel Settings<small>Configure general settings for hPanel.</small></h1>
     <ol class="breadcrumb">
         <li><a href="{{ route('admin.index') }}">Admin</a></li>
         <li class="active">Jexactyl</li>
@@ -25,80 +25,10 @@
                 @endif
             ">
                 <div class="box-header with-border">
-                    <i class="fa fa-code-fork"></i> <h3 class="box-title">Software Release <small>Verify Jexactyl is up-to-date.</small></h3>
+                    <i class="fa fa-code-fork"></i> <h3 class="box-title">hPanel Version <small>Verify hPanel is up-to-date.</small></h3>
                 </div>
                 <div class="box-body">
-                    @if ($version->isLatestPanel())
-                        You are running Jexactyl <code>{{ config('app.version') }}</code>. 
-                    @else
-                        Jexactyl is not up-to-date. Latest release is available <a href="https://github.com/jexactyl/jexactyl/releases/v{{ $version->getPanel() }}" target="_blank">here</a>.
-                    @endif
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-xs-12">
-            <div class="col-xs-12 col-md-3">
-                <div class="info-box">
-                    <span class="info-box-icon"><i class="fa fa-server"></i></span>
-                    <div class="info-box-content" style="padding: 23px 10px 0;">
-                        <span class="info-box-text">Total Servers</span>
-                        <span class="info-box-number">{{ count($servers) }}</span>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xs-12 col-md-3">
-                <div class="info-box">
-                    <span class="info-box-icon"><i class="fa fa-wifi"></i></span>
-                    <div class="info-box-content" style="padding: 23px 10px 0;">
-                        <span class="info-box-text">Total Allocations</span>
-                        <span class="info-box-number">{{ $allocations }}</span>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xs-12 col-md-3">
-                <div class="info-box">
-                    <span class="info-box-icon"><i class="fa fa-pie-chart"></i></span>
-                    <div class="info-box-content" style="padding: 23px 10px 0;">
-                        <span class="info-box-text">Total RAM use</span>
-                        <span class="info-box-number">{{ $used['memory'] }} MB of {{ $available['memory'] }} MB</span>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xs-12 col-md-3">
-                <div class="info-box">
-                    <span class="info-box-icon"><i class="fa fa-hdd-o"></i></span>
-                    <div class="info-box-content" style="padding: 23px 10px 0;">
-                        <span class="info-box-text">Total disk use</span>
-                        <span class="info-box-number">{{ $used['disk'] }} MB of {{ $available['disk'] }} MB </span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-xs-12">
-            <div class="box box-primary">
-                <div class="box-header with-border">
-                    <i class="fa fa-bar-chart"></i> <h3 class="box-title">Resource Utilization <small>A glance of the total amount of resources used.</small></h3>
-                </div>
-                <div class="box-body">
-                    <div class="col-xs-12 col-md-4">
-                        <canvas id="servers_chart" width="100%" height="50">
-                            <p class="text-muted">No data is available for this chart.</p>
-                        </canvas>
-                    </div>
-                    <div class="col-xs-12 col-md-4">
-                        <canvas id="ram_chart" width="100%" height="50">
-                            <p class="text-muted">No data is available for this chart.</p>
-                        </canvas>
-                    </div>
-                    <div class="col-xs-12 col-md-4">
-                        <canvas id="disk_chart" width="100%" height="50">
-                            <p class="text-muted">No data is available for this chart.</p>
-                        </canvas>
-                    </div>
+                       You are running hPanel ={{ config('app.version') }}. 
                 </div>
             </div>
         </div>
